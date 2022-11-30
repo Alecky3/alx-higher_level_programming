101ee0c98ab8b10c25c7b15c5f4dec012f453739
alex <alexmuthini3@gmail.com>
2022-11-30 21:39:31 +0300

    added 13-insert_number.c file

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,34 @@
+
+#include "lists.h"
+
+/**
+ * insert_current - inserts a number into a sorted singly-linked list.
+ * @head: A pointer the head of the linked list.
+ * @number: The number to insert.
+ * Return: If the function fails return NULL.
+ * Otherwise return a pointer to the new current.
+ */
+listint_t *insert_current(listint_t **head, int number)
+{
+	listint_t *current = *head, *new;
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = number;
+
+	if (current == NULL || current->n >= number)
+	{
+		new->next = current;
+		*head = new;
+		return (new);
+	}
+
+	while (current && current->next && current->next->n < number)
+		current = current->next;
+
+	new->next = current->next;
+	current->next = new;
+
+	return (new);
+}
