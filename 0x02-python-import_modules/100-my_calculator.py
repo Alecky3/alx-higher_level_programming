f39d413a1018bbcc50a0b5b0f372db3c072cceb0
alex <alexmuthini3@gmail.com>
2022-12-01 16:13:53 +0300

    corrected requirement fail

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
 
-    if sys.argv[2] not in list(["+","-","*","/"]):
+    if sys.argv[2] not in list(operators.keys()):
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
     
