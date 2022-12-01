a30fea388632461ad3d96114d3025d2c3584deda
alex <alexmuthini3@gmail.com>
2022-12-01 16:28:29 +0300

    corrected requirement fail

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -4,15 +4,16 @@ if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
     import sys
     
-    operators = {"+": add, "-": sub, "*": mul, "/": div}
     if len(sys.argv) - 1 != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
-
-    if sys.argv[2] not in list(operators.keys()):
+    
+    if sys.argv[2] not in ['+','-','*','/']:
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
     
     a = int(sys.argv[1])
     b = int(sys.argv[3])
-    print("{} {} {} = {}".format(a, sys.argv[2], b, operators[sys.argv[2]](a, b)))
+
+    operators = {"+": add, "-": sub, "*": mul, "/": div}
+    print("{0} {1} {2} = {3}".format(a, sys.argv[2], b, operators[sys.argv[2]](a, b)))
