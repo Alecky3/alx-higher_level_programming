5bed1d072b4c4a35fe1792f6cce61b261df200f1
alex <alexmuthini3@gmail.com>
2022-12-01 15:50:21 +0300

    corrected requirement fail

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-
 if __name__ == "__main__":
     import hidden_4
 
     hidden_4Names = dir(hidden_4)
     for name in hidden_4Names:
-        if not name.startsWith('__'):
+        if name[:2] != "__":
             print(name)
