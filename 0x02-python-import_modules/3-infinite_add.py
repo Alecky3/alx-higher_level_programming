4a98201dbfd98899f39f240956b7798be161ee54
alex <alexmuthini3@gmail.com>
2022-12-01 15:26:45 +0300

    added 3-infinite_add.py

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+
+    import sys
+
+    argsCount = len(sys.argv)
+    sum = 0
+
+    for i in range(1,argsCount):
+        sum += int(sys.argv[i])
+
+    print("{}".format(sum))
