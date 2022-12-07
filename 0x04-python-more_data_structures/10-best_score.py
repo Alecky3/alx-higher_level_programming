8946209a3050536a0b63315142b0caf4e00ca3b5
alex <alexmuthini3@gmail.com>
2022-12-07 16:20:52 +0300

    corrected requirement fail v3

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,15 +2,13 @@
 
 def best_score(a_dictionary):
     """Returns a key with the biggest integer value."""
-
     if not isinstance(a_dictionary, dict) or len(a_dictionary) == 0:
-        return (None)
+        return None
 
-    return_key = list(a_dictionary.keys())[0]
-    max_item = a_dictionary[return_key]
-    for key, value in a_dictionary:
+    retKey = list(a_dictionary.keys())[0]
+    max_item = a_dictionary[retKey]
+    for key, value in a_dictionary.items():
         if value > max_item:
             max_item = value
-            return_key = key
-
-    return (return_key)
+            retKey = key
+    return (retKey)
