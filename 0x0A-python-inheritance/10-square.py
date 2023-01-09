5cd22b79f72e0c12debfab990213738be50c80c2
alex <alexmuthini3@gmail.com>
2023-01-09 22:00:42 +0300

    added 10-square.py file

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Defines Square Class."""
-Rectangle = __import__("9-rectangle")
+Rectangle = __import__("9-rectangle").Rectangle
 
 
 class Square(Rectangle):
@@ -12,5 +12,6 @@ class Square(Rectangle):
         Args:
          size (int): the size of the square
         """
+        super().integer_validator("size", size)
         super().__init__( size, size)
         self.__size = size
