10089025d653fee50c0abf708e3123d7d4fb8ca5
alex <alexmuthini3@gmail.com>
2023-01-10 21:37:06 +0300

    added 11-student.py file

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Defines Student class."""
+
+
+class Student:
+    """Represents Student class."""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieves a dictionary of a student instance."""
+        if (type(attrs) == list and
+                all(type(elem) == str form elem in attrs)):
+            return {elem:getattr(self, elem) for elem in attrs if
+                    hasattr(self, elem)}
+        return self.__dict__
+    def reload_from_json(self, json):
+        """Replaces all attributes of the Student instance."""
+        for key, value in json.items():
+            setattr(self, key, value)
