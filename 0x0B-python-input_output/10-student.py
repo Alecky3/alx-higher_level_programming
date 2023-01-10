c986e0b0adfc8cc494b735806116afa462277f57
alex <alexmuthini3@gmail.com>
2023-01-10 21:33:46 +0300

    added 10-student.py file

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Defines Student class."""
+
+
+class Student:
+    """Represents Student class."""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieves a dictionary of a student instance."""
+        if (type(attrs) == list and
+                all(type(elem) == str form elem in attrs)):
+            return {elem:getattr(self, elem) for elem in attrs if
+                    hasattr(self, elem)}
+        return self.__dict__
