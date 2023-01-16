0ad84e927618abd833fbb034cc295b2253e885b8
alex <alexmuthini3@gmail.com>
2023-01-16 16:52:34 +0300

    changed Square by adding public method to_dictionary

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -70,3 +70,7 @@ class Square(Rectangle):
                     self.x = value
                 elif key == "y":
                     self.y = value
+
+    def to_dictionary(self):
+        """Returns the dictionary representation of a Square."""
+        return {"id": self.id, "x": self.x, "size": self.size, "y": self.y}
