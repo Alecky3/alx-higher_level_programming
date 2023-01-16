bcb487eeaec3604c2771ca3ddc6913ea2bbc427b
alex <alexmuthini3@gmail.com>
2023-01-16 17:03:54 +0300

    changed base ny adding static method to_json_string

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """Contains Base class definition."""
+import json
 
 
 class Base:
@@ -20,3 +21,17 @@ class Base:
         else:
             type(self).__nb_objects += 1
             self.id = type(self).__nb_objects
+    
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Returns the JSON string representation of list_dictionaries
+
+        Args:
+          list_dictionaries (list): list of dictionaries
+
+        Returns: JSON string
+        """
+
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        return json.dumps(list_dictionaries)
