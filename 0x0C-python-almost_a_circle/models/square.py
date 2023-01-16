0a92c7c6773d4fc023b393f2bf55bb791a2329fb
alex <alexmuthini3@gmail.com>
2023-01-16 16:15:22 +0300

    corrected syntax error in square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -15,7 +15,7 @@ class Square(Rectangle):
     """
 
     def __init__(self, size, x=0, y=0, id=None):
-        supe().__init__(size, size, x, y, id)
+        super().__init__(size, size, x, y, id)
 
     def __str__(self):
         """Retruns the print() and str() representation of Square."""
