7dfe51402438ceac01a91952cd80aab472ed6fa7
alex <alexmuthini3@gmail.com>
2023-01-16 16:28:26 +0300

    Updated square by adding public update method

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -32,3 +32,41 @@ class Square(Rectangle):
     def size(self, width):
         self.width = width
         self.height = width
+
+    def update(self, *args, **kwargs):
+        """Assigns an argument to each attribute
+
+        Args:
+          *args (list): the argumets to use for updating the attributes
+          **kwargs (dict): the arguments to use for updating the attribues
+                           when *args are not present
+        """
+        count = 0
+        if args and len(args) != 0:
+            for arg in args:
+                if count == 0:
+                    if arg is None:
+                        self.__init__(self.size, self.x, self.y)
+                    else:
+                        self.id = arg
+                elif count == 1:
+                    self.size = arg
+                elif count == 2:
+                    self.x = arg
+                elif count == 3:
+                    self.y = arg
+                count += 1
+
+        elif kwargs and len(kwargs) != 0:
+            for key, value in kwargs.items():
+                if key == "id":
+                    if value is None:
+                        self.__init__(self.size, self.x, self.y)
+                    else:
+                        self.id = value
+                elif key == "size":
+                    self.size = value
+                elif key == "x":
+                    self.x = value
+                elif key == "y":
+                    self.y = value
