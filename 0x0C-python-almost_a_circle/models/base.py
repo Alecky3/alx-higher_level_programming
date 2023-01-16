ba2bbbb2ef9b992af0bca1cf4f0d212d31282564
alex <alexmuthini3@gmail.com>
2023-01-16 21:48:20 +0300

    updated Base by adding static method from_json_string

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -48,3 +48,15 @@ class Base:
             else:
                 list_dictionaries = [obj.to_dictionary() for obj in list_objs]
                 f.write(Base.to_json_string(list_dictionaries))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """Returns the list of the JSON string representation json_string.
+
+        Args:
+          json_string (str): string representing a list of dictionaries.
+        """
+
+        if json_string is None or json_string == "[]":
+            return []
+        return json.loads(json_string)
