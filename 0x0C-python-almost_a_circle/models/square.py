1d4c396583ad49b11fa7352f90493859563bb7ab
alex <alexmuthini3@gmail.com>
2023-01-16 16:11:10 +0300

    added square.py file

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Defines the class Square which is a subclass of Rectangle."""
+from rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Represents Square which inherits Rectangle
+
+    Args:
+      size (int): size of the square
+      x (int): x coordinate
+      y (int): y coordinate
+      id (int): identifies the square instance
+
+    """
+
+    def __init__(self, size, x=0, y=0, id=None):
+        supe().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """Retruns the print() and str() representation of Square."""
+
+        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y,
+                                                 self.width)
