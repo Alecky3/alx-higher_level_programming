ce0785d742ec8c416edda636c920481ff5479b71
alex <alexmuthini3@gmail.com>
2023-01-16 16:54:31 +0300

    corrected pycodestyle errors in rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -146,10 +146,5 @@ class Rectangle(Base):
 
     def to_dictionary(self):
         """Returns the dictionary representation of a Rectangle."""
-        return {
-            "x": self.x,
-            "y": self.y,
-            "id": self.id,
-            "height": self.height,
-            "width" : self.width,
-            }
+        return {"x": self.x, "y": self.y, "id": self.id, "height": self.height,
+                "width": self.width}
