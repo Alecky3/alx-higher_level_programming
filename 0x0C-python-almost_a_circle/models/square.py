656e982f207e30fd56a5d562675e2465c96bc9c7
alex <alexmuthini3@gmail.com>
2023-01-19 10:34:49 +0300

    corrected import error in square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Defines the class Square which is a subclass of Rectangle."""
-from rectangle import Rectangle
+from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
