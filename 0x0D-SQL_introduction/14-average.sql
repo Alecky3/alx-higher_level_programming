55999bc3fc2b41154e9a261adfff01d5e37e1f65
alex <alexmuthini3@gmail.com>
2023-02-14 21:35:11 +0300

    added 14-average

---
+++ 0x0D-SQL_introduction/14-average.sql
@@ -0,0 +1,3 @@
+-- Computes the average score of all records in the table 'second_table'
+-- of the database hbtn_0c_0, the column name should be 'average'.
+SELECT AVG(`score`) AS `average` FROM `second_table`;
