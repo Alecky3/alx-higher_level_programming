d96fb969e658e8803c8920800d1259a7bd49dd45
alex <alexmuthini3@gmail.com>
2023-02-14 21:39:31 +0300

    added 16-no_link

---
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -0,0 +1,5 @@
+-- Lists all records of the table 'second_table' having a name value.
+-- Records are ordered by 'score' in  descending order.
+SELECT `score`, `name` FROM `second_table`
+WHERE `name` != ""
+ORDER BY `score` DESC
