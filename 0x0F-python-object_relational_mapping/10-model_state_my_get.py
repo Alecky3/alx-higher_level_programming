b49b12fee408a89aa4dd8b3ede10f4c47d18870d
alex <alexmuthini3@gmail.com>
2023-03-18 17:22:49 +0300

    added 10-model_state_my_get.py

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Lists all 'State' objects from the database 'hbtn_0e_6_usa'."""
+from model_state import Base, State
+from sqlalchemy import create_engine
+import sys
+from urllib.parse import quote_plus
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+                            sys.argv[1], quote_plus(sys.argv[2]),
+                            sys.argv[3]))
+    Session = sessionmaker(bind=engine)
+
+    session = Session()
+
+    q = session.query(State).order_by(State.id).all()
+    res = [s.id for s in q if s.name == sys.argv[4]]
+    if res:
+        for s in res:
+            print("{}".format(s))
+    else:
+        print("Not found")
