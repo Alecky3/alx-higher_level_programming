d8efabbf8e2716a0df4b61aaf84d9ac74b597193
alex <alexmuthini3@gmail.com>
2023-03-31 16:22:28 +0300

    added 2-post_email.py

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -9,5 +9,5 @@ import urllib.request
 if __name__ == "__main__":
     url = sys.argv[1]
     req = urllib.request.Request(url)
-    with urllib.request.urlopen(req) as response:
+    with urllib.request.urlopen(req) as res:
         print(req.get_header('X-Request-Id'))
