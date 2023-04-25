58d61c4ab66e23fcd96299d83aaddb308cde033e
alex <alexmuthini3@gmail.com>
2023-04-25 23:01:16 +0300

    modified 5-request_store

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -2,6 +2,6 @@
 const fs = require('fs');
 const req = require('request');
 
-const url = process.argv[2]
-const filename = process.argv[3]
-request(url).pipe(fs.createWriteStream(filename));
+const url = process.argv[2];
+const filename = process.argv[3];
+req(url).pipe(fs.createWriteStream(filename));
